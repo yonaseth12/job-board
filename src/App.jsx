09d6c4c13e61dbee65284e9fb52f5321a374340f
Yonas W <yonaseth12@gmail.com>
2025-03-04 15:12:24 +0300

    define app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './App.css'
+import AppLayout from './layouts/app-layout'
+import LandingPage from './pages/landing'
+import Onboarding from './pages/onboarding'
+import JobListing from './pages/job-listing'
+import JobPage from './pages/job'
+import PostJob from './pages/post-job'
+import SavedJobs from './pages/saved-jobs'
+import MyJobs from './pages/my-jobs'
 
+const router =createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />
+      },
+      {
+        path: '/onboarding',
+        element: <Onboarding />,
+      },
+      {
+        path: '/jobs',
+        element: <JobListing />,
+      },
+      {
+        path: '/job/:id',
+        element: <JobPage />,
+      },
+      {
+        path: '/post-job',
+        element: <PostJob />,
+      },
+      {
+        path: '/saved-job',
+        element: <SavedJobs />,
+      },
+      {
+        path: '/my-jobs',
+        element: <MyJobs />,
+      },
+    ]
+  }
+])
 function App() {
 
   return (
-    <> 
-      <div>Job Board React App</div>
-    </>
+    <RouterProvider router={router}/>    
   )
 }
 
---
+++ src/pages/onboarding.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Onboadring = () => {
+  return (
+    <div>Onboadring</div>
+  )
+}
+
+export default Onboadring
